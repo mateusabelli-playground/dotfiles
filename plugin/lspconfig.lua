d89ca5aae6462dc57aa9cd2d5a7376c0471e79ee
Mateus Abelli <43862225+mateusabelli@users.noreply.github.com>
2022-09-04 21:25:34 -0300

    fix(nvim) add missing protected call
    - Added protected call to avoid error upon first nvim initialization without plugins installed

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -37,6 +37,9 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 
+local status_c, _ = pcall(require, "cmp_nvim_lsp")
+if (not status_c) then return end
+
 local capabilities = require('cmp_nvim_lsp').update_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
